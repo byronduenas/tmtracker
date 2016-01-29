3abb111bacc67937b76ab36bbd7d86b0b511b7f6
Byron Duenas <byronduenas@gmail.com>
2016-01-28 18:08:36 -0800

    Initial Commit

---
+++ TMTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMTracker
+//
+//  Created by Byron Duenas on 2016-01-28.
+//  Copyright © 2016 Em C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
